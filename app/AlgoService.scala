ffc14aaf2e8c9a738d7d6febf8de27829608ed75
yllan <yllan@me.com>
2013-07-27 01:10:35 +0800

    Fix the output JSON format

--- app/AlgoService.scala
+++ app/AlgoService.scala
@@ -31,41 +31,43 @@ object AlgoService extends Controller {
     Ok("Welcome to algo service.")
   }
   
-  def interpolate = Action { implicit req =>
-    val minLongitude = req.queryString.get("min_long").map(l => try {l.head.toDouble} catch { case t: Throwable => 120.0}).getOrElse(120.0)
-    val maxLongitude = req.queryString.get("max_long").map(l => try {l.head.toDouble} catch { case t: Throwable => 122.5}).getOrElse(122.5)
-    val minLatitude = req.queryString.get("min_lat").map(l => try {l.head.toDouble} catch { case t: Throwable => 22.0}).getOrElse(22.0)
-    val maxLatitude = req.queryString.get("max_lat").map(l => try {l.head.toDouble} catch { case t: Throwable => 25.5}).getOrElse(22.5)
-    val epsilon = req.queryString.get("epsilon").map(l => try {l.head.toDouble} catch { case t: Throwable => 0.02}).getOrElse(0.02)
+  def interpolate = Action { implicit req ⇒
+    val minLongitude = req.queryString.get("min_long").map(l ⇒ try {l.head.toDouble} catch { case t: Throwable ⇒ 120.0}).getOrElse(120.0)
+    val maxLongitude = req.queryString.get("max_long").map(l ⇒ try {l.head.toDouble} catch { case t: Throwable ⇒ 122.5}).getOrElse(122.5)
+    val minLatitude = req.queryString.get("min_lat").map(l ⇒ try {l.head.toDouble} catch { case t: Throwable ⇒ 22.0}).getOrElse(22.0)
+    val maxLatitude = req.queryString.get("max_lat").map(l ⇒ try {l.head.toDouble} catch { case t: Throwable ⇒ 25.5}).getOrElse(22.5)
+    val epsilon = req.queryString.get("epsilon").map(l ⇒ try {l.head.toDouble} catch { case t: Throwable ⇒ 0.02}).getOrElse(0.02)
     val selector = req.queryString.get("selector").map(_.head).getOrElse("today")
 
-    (for (url <- req.queryString.get("url").map(_.head))
+    (for (url <- req.queryString.get("from").map(_.head))
      yield {
-      Logger.info("url: " + url)
+      Logger.info("Calculate interpolation from " + url)
     
       val shepardF: Future[Shep_interp] =
-        WS.url(url).get.map(req => try { req.json } catch { case t: Throwable => Json.obj() }).flatMap((data: JsValue) => {
+        WS.url(url).get.map(req ⇒ try { req.json } catch { case t: Throwable ⇒ Json.obj() }).flatMap((data: JsValue) ⇒ {
         import g0v.Geo._
 
         val rainfallSamples: Seq[(Double, Double, Double)] =
           (for (kv <- data.as[Map[String, Map[String, String]]];
                loc <- stations.get(kv._1))
           yield
-            (loc.longitude, loc.latitude, try { (kv._2)(selector).toDouble } catch { case t: Throwable => 0.0 })
+            (loc.longitude, loc.latitude, try { (kv._2)(selector).toDouble } catch { case t: Throwable ⇒ 0.0 })
           ).toSeq
         implicit val timeout = Timeout(120.seconds)
-        Logger.info("samples calculated!")
+        Logger.info("Shepard calculated!")
         (shepardActor ask rainfallSamples).mapTo[Shep_interp]
       })
 
       val (enumerator, channel) = Concurrent.broadcast[String]
 
-      shepardF.map(shepard => {
+      shepardF.map(shepard ⇒ {
+        channel.push("[")
       
         def generateGrids(longitude: Double, latitude: Double, first: Boolean): Unit = {
           val z = shepard.interp(Array(longitude, latitude))
           channel.push((if (!first) "," else "") + f"[$longitude%.4f,$latitude%.4f,$z%.4f]")
           if (longitude >= maxLongitude && latitude >= maxLatitude) {
+            Logger.info("Interpolation Done.")
             channel.push("]")
             channel.eofAndEnd()
           } else if (latitude >= maxLatitude)
@@ -77,7 +79,6 @@ object AlgoService extends Controller {
         generateGrids(minLongitude, minLatitude, true)
       })
 
-      channel.push("[")
       Ok.stream(enumerator)
 
     }).getOrElse(Ok("Ooops")) // Error, return empty result
--- app/g0v/ShepardInterpolationActor.scala
+++ app/g0v/ShepardInterpolationActor.scala
@@ -6,10 +6,9 @@ import com.nr.interp._
 
 class ShepardInterpolationActor extends Actor with ActorLogging {
   def receive = {
-    case (samples: Seq[(Double, Double, Double)]) => {
-      log.info("YES!")
-      val ptss: Array[Array[Double]] = samples.map { tuple => Array(tuple._1, tuple._2) }.toArray
-      val valss: Array[Double] = samples.map { tuple => tuple._3 }.toArray
+    case (samples: Seq[(Double, Double, Double)]) ⇒ {
+      val ptss: Array[Array[Double]] = samples.map { tuple ⇒ Array(tuple._1, tuple._2) }.toArray
+      val valss: Array[Double] = samples.map { tuple ⇒ tuple._3 }.toArray
       val shepard = new Shep_interp(ptss, valss)
 
       sender ! shepard
